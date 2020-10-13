4ddd9c17644ee091817eca90ea887e9ce914b77f
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-12 22:05:17 -0400

    Switch to using BiocFileCache

--- R/AllGenerators.R
+++ R/AllGenerators.R
@@ -1,6 +1,6 @@
+#' @name PANTHER
 #' @inherit PANTHER-class title description return
-#' @note Updated 2019-08-27.
-#' @export
+#' @note Updated 2020-10-12.
 #'
 #' @section Suported organisms:
 #'
@@ -16,143 +16,21 @@
 #'   server (e.g. `"14.0"`).
 #'
 #' @examples
-#' options(acid.test = TRUE)
 #' x <- PANTHER(
 #'     organism = "Homo sapiens",
 #'     BPPARAM = BiocParallel::SerialParam()
 #' )
 #' print(x)
-PANTHER <- function(  # nolint
-    organism,
-    release = NULL,
-    BPPARAM = BiocParallel::SerialParam(progressbar = TRUE)  # nolint
-) {  # nolint
-    assert(
-        hasInternet(),
-        isString(organism)
-    )
-    organism <- match.arg(
-        arg = organism,
-        choices = names(.pantherMappings)
-    )
-    pantherName <-  .pantherMappings[[organism]]
-    assert(isString(pantherName))
-    if (is.null(release)) {
-        release <- "current_release"
-    }
-    assert(isString(release))
-    release <- match.arg(arg = release, choices = .pantherReleases)
-    message(sprintf(
-        "Downloading PANTHER annotations for '%s' (%s).",
-        organism,
-        gsub("_", " ", release)
-    ))
-    if (isTRUE(getOption("acid.test"))) {
-        file <- pasteURL(
-            basejumpTestsURL, paste0("PTHR13.1_", pantherName, ".gz"),
-            protocol = "none"
-        )
-    } else {
-        ## FTP connection is unreliable for CI, so cover locally instead.
-        ## nocov start
-        file <- transmit(
-            remoteDir = pasteURL(
-                "ftp.pantherdb.org",
-                "sequence_classifications",
-                release,
-                "PANTHER_Sequence_Classification_files",
-                protocol = "ftp"
-            ),
-            pattern = pantherName,
-            compress = TRUE,
-            localDir = tempdir()
-        )
-        ## nocov end
-    }
-    data <- import(
-        file = file,
-        format = "tsv",
-        colnames = c(
-            "pantherID",
-            "X2",
-            "pantherSubfamilyID",
-            "pantherFamilyName",
-            "pantherSubfamilyName",
-            "goMF",
-            "goBP",
-            "goCC",
-            "pantherClass",
-            "pantherPathway"
-        )
-    )
-    data[["X2"]] <- NULL
-    data <- as(data, "DataFrame")
-    ## Now using base R methods here instead of `tidyr::separate()`.
-    idsplit <- strsplit(data[["pantherID"]], split = "|", fixed = TRUE)
-    idsplit <- DataFrame(do.call(what = rbind, args = idsplit))
-    colnames(idsplit) <- c("organism", "keys", "uniprotKB")
-    data[["pantherID"]] <- NULL
-    data[["keys"]] <- idsplit[["keys"]]
-    ## Using organism-specific internal return functions here.
-    fun <- get(paste("", "PANTHER", camelCase(organism), sep = "."))
-    assert(is.function(fun))
-    data <- fun(data)
-    assert(
-        is(data, "DataFrame"),
-        hasRows(data)
-    )
-    data[["keys"]] <- NULL
-    data <- data[, unique(c("geneID", colnames(data)))]
-    keep <- !is.na(data[["geneID"]])
-    data <- data[keep, , drop = FALSE]
-    data <- unique(data)
-    ## Some organisms have duplicate PANTHER annotations per gene ID.
-    split <- split(data, f = data[["geneID"]])
-    split <- SplitDataFrameList(bplapply(
-        X = split,
-        FUN = function(x) {
-            x <- x[order(x[["pantherSubfamilyID"]]), , drop = FALSE]
-            x <- head(x, n = 1L)
-            x
-        },
-        BPPARAM = BPPARAM
-    ))
-    data <- unsplit(split, f = unlist(split[, "geneID"]))
-    data <- data[order(data[["geneID"]]), , drop = FALSE]
-    assert(hasNoDuplicates(data[["geneID"]]))
-    message("Splitting and sorting the GO terms.")
-    data <- mutateAt(
-        object = data,
-        vars = c(
-            "goBP",
-            "goCC",
-            "goMF",
-            "pantherClass",
-            "pantherPathway"
-        ),
-        fun = .splitPANTHERTerms,
-        BPPARAM = BPPARAM
-    )
-    ## Sort columns alphabetically.
-    data <- data[, sort(colnames(data)), drop = FALSE]
-    rownames(data) <- data[["geneID"]]
-    metadata(data) <- list(
-        version = packageVersion(packageName()),
-        date = Sys.Date()
-    )
-    metadata(data)[["organism"]] <- organism
-    metadata(data)[["release"]] <- release
-    new(Class = "PANTHER", data)
-}
+NULL
 
 
 
-## Updated 2019-07-22.
+## Updated 2020-10-12.
 .pantherMappings <- c(
-    "Caenorhabditis elegans" = "nematode_worm",
-    "Drosophila melanogaster" = "fruit_fly",
     "Homo sapiens" = "human",
-    "Mus musculus" = "mouse"
+    "Mus musculus" = "mouse",
+    "Caenorhabditis elegans" = "nematode_worm",
+    "Drosophila melanogaster" = "fruit_fly"
 )
 
 
@@ -183,6 +61,120 @@ PANTHER <- function(  # nolint
 
 
 
+#' @rdname PANTHER
+#' @export
+PANTHER <-
+    function(  # nolint
+        organism,
+        release = NULL,
+        BPPARAM = BiocParallel::bpparam()  # nolint
+    ) {
+        assert(hasInternet())
+        organism <- match.arg(organism)
+        pantherName <-  .pantherMappings[[organism]]
+        assert(isString(pantherName))
+        if (is.null(release)) {
+            release <- "current_release"
+        }
+        assert(isString(release))
+        release <- match.arg(arg = release, choices = .pantherReleases)
+        message(sprintf(
+            "Downloading PANTHER annotations for '%s' (%s).",
+            organism,
+            gsub("_", " ", release)
+        ))
+        url <- transmit(
+            remoteDir = pasteURL(
+                "ftp.pantherdb.org",
+                "sequence_classifications",
+                release,
+                "PANTHER_Sequence_Classification_files",
+                protocol = "ftp"
+            ),
+            pattern = pantherName,
+            download = FALSE
+        )
+        file <- cacheURL(url = url, pkg = packageName())
+        data <- import(
+            file = file,
+            format = "tsv",
+            colnames = c(
+                "pantherID",
+                "X2",
+                "pantherSubfamilyID",
+                "pantherFamilyName",
+                "pantherSubfamilyName",
+                "goMF",
+                "goBP",
+                "goCC",
+                "pantherClass",
+                "pantherPathway"
+            )
+        )
+        data[["X2"]] <- NULL
+        data <- as(data, "DataFrame")
+        ## Now using base R methods here instead of `tidyr::separate()`.
+        idsplit <- strsplit(data[["pantherID"]], split = "|", fixed = TRUE)
+        idsplit <- DataFrame(do.call(what = rbind, args = idsplit))
+        colnames(idsplit) <- c("organism", "keys", "uniprotKB")
+        data[["pantherID"]] <- NULL
+        data[["keys"]] <- idsplit[["keys"]]
+        ## Using organism-specific internal return functions here.
+        fun <- get(paste("", "PANTHER", camelCase(organism), sep = "."))
+        assert(is.function(fun))
+        data <- fun(data)
+        assert(
+            is(data, "DataFrame"),
+            hasRows(data)
+        )
+        data[["keys"]] <- NULL
+        data <- data[, unique(c("geneID", colnames(data)))]
+        keep <- !is.na(data[["geneID"]])
+        data <- data[keep, , drop = FALSE]
+        data <- unique(data)
+        ## Some organisms have duplicate PANTHER annotations per gene ID.
+        split <- split(data, f = data[["geneID"]])
+        split <- SplitDataFrameList(bplapply(
+            X = split,
+            FUN = function(x) {
+                x <- x[order(x[["pantherSubfamilyID"]]), , drop = FALSE]
+                x <- head(x, n = 1L)
+                x
+            },
+            BPPARAM = BPPARAM
+        ))
+        data <- unsplit(split, f = unlist(split[, "geneID"]))
+        data <- data[order(data[["geneID"]]), , drop = FALSE]
+        assert(hasNoDuplicates(data[["geneID"]]))
+        message("Splitting and sorting the GO terms.")
+        data <- mutateAt(
+            object = data,
+            vars = c(
+                "goBP",
+                "goCC",
+                "goMF",
+                "pantherClass",
+                "pantherPathway"
+            ),
+            fun = .splitPANTHERTerms,
+            BPPARAM = BPPARAM
+        )
+        ## Sort columns alphabetically.
+        data <- data[, sort(colnames(data)), drop = FALSE]
+        rownames(data) <- data[["geneID"]]
+        metadata(data) <- list(
+            version = packageVersion(packageName()),
+            date = Sys.Date()
+        )
+        metadata(data)[["organism"]] <- organism
+        metadata(data)[["release"]] <- release
+        new(Class = "PANTHER", data)
+    }
+
+formals(PANTHER)[["organism"]] <- names(.pantherMappings)
+
+
+
 ## Updated 2020-01-09.
 .splitPANTHERTerms <- function(x, BPPARAM) {  # nolint
     assert(is.atomic(x))
@@ -209,9 +201,37 @@ PANTHER <- function(  # nolint
 
 
 ## Updated 2019-08-16.
+.PANTHER.caenorhabditisElegans <-  # nolint
+    function(data) {
+        data[["geneID"]] <- str_extract(
+            string = data[["keys"]],
+            pattern = "WBGene\\d{8}$"
+        )
+        data
+    }
+
+
+
+## Updated 2019-08-16.
+.PANTHER.drosophilaMelanogaster <-  # nolint
+    function(data) {
+        data[["geneID"]] <- str_extract(
+            string = data[["keys"]],
+            pattern = "FBgn\\d{7}$"
+        )
+        data
+    }
+
+
+
+## Updated 2020-10-12.
 .PANTHER.homoSapiens <-  # nolint
     function(data) {
-        hgnc2ensembl <- HGNC2Ensembl()
+        h2e <- HGNC2Ensembl()
+        assert(identical(colnames(h2e), c("hgnc", "ensembl")))
+        h2e <- as(h2e, "DataFrame")
+        colnames(h2e)[colnames(h2e) == "hgnc"] <- "hgncID"
+        colnames(h2e)[colnames(h2e) == "ensembl"] <- "geneID"
         ## Filter Ensembl matches.
         ensembl <- data
         pattern <- "ENSG[0-9]{11}"
@@ -227,7 +247,7 @@ PANTHER <- function(  # nolint
         hgnc[["hgncID"]] <- as.integer(
             str_match(string = hgnc[["keys"]], pattern = pattern)[, 2L]
         )
-        hgnc <- leftJoin(hgnc, hgnc2ensembl, by = "hgncID")
+        hgnc <- leftJoin(hgnc, h2e, by = "hgncID")
         hgnc[["hgncID"]] <- NULL
         keep <- !is.na(hgnc[["geneID"]])
         hgnc <- hgnc[keep, , drop = FALSE]
@@ -264,28 +284,4 @@ PANTHER <- function(  # nolint
         mgi <- unique(mgi)
         ## Bind and return.
         do.call(what = rbind, args = list(ensembl, mgi))
-    }
-
-
-
-## Updated 2019-08-16.
-.PANTHER.drosophilaMelanogaster <-  # nolint
-    function(data) {
-        data[["geneID"]] <- str_extract(
-            string = data[["keys"]],
-            pattern = "FBgn\\d{7}$"
-        )
-        data
-    }
-
-
-
-## Updated 2019-08-16.
-.PANTHER.caenorhabditisElegans <-  # nolint
-    function(data) {
-        data[["geneID"]] <- str_extract(
-            string = data[["keys"]],
-            pattern = "WBGene\\d{8}$"
-        )
-        data
     }
