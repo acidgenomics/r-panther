d2f7e183d7b6a1fbfeab62e7b227c5d26446ee61
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-02 14:34:03 -0500

    NAMESPACE update

--- R/package.R
+++ R/package.R
@@ -4,4 +4,17 @@
 #'
 #' @aliases NULL
 #' @keywords internal
+#'
+#' @importFrom BiocParallel bplapply
+#' @importFrom IRanges SplitDataFrameList unsplit
+#' @importFrom S4Vectors DataFrame SimpleList do.call head metadata metadata<-
+#'   split
+#' @importFrom basejump HGNC2Ensembl MGI2Ensembl alert basejumpTestsURL cacheURL
+#'   camelCase import leftJoin mutateAt pasteURL showHeader showSlotInfo
+#'   transmit
+#' @importFrom goalie assert hasInternet hasLength hasNoDuplicates hasRows
+#'   isString isSubset validate
+#' @importFrom methods as is new setClass setValidity show
+#' @importFrom stringr str_detect str_extract str_match
+#' @importFrom utils packageName packageVersion
 "_PACKAGE"
