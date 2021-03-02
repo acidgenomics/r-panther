dfa99a7d8734f7571f35c45b7979d9f3ac8393a4
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-02 17:45:28 -0500

    Update unit test

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,5 @@ library(testthat)
 library(PANTHER)
 test_check("PANTHER")
 if (isTRUE(getOption("acid.test.extra"))) {
-    options(acid.test = FALSE)
     test_dir(file.path("tests", "testthat-extra"))
 }
