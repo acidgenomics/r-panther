3ae236c6a68c6915c588be8803bc647375b1cabd
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-03 13:30:39 -0400

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -6,10 +6,10 @@
 #' @keywords internal
 #'
 #' @importFrom basejump CharacterList DataFrame HGNC2Ensembl MGI2Ensembl
-#'   SimpleList SplitDataFrameList alert alertWarning basejumpTestsURL cacheURL
-#'   camelCase do.call gsub head import lapply leftJoin metadata metadata<-
-#'   mutateAt packageName packageVersion pasteURL rbind showHeader showSlotInfo
-#'   sort split str_detect str_extract str_match tail unique unlist unsplit
+#'   SimpleList SplitDataFrameList alert alertWarning cacheURL camelCase do.call
+#'   gsub head import lapply leftJoin metadata metadata<- mutateAt packageName
+#'   packageVersion pasteURL rbind showHeader showSlotInfo sort split str_detect
+#'   str_extract str_match tail unique unlist unsplit
 #' @importFrom goalie assert hasInternet hasLength hasNoDuplicates hasRows
 #'   isOrganism isString isSubset validate
 #' @importFrom methods as is new setClass setValidity show
