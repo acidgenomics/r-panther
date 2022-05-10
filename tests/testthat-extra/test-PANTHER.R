f53d93f055b66ab08378aa60e77a80b509dce565
Michael Steinbaugh <mike@steinbaugh.com>
2022-05-10 16:23:30 -0400

    Update unit tests

--- tests/testthat-extra/test-PANTHER.R
+++ tests/testthat-extra/test-PANTHER.R
@@ -11,8 +11,8 @@ test_that("PANTHER : 11.0", {
         nrow = c(
             "Caenorhabditis elegans" = 15010L,
             "Drosophila melanogaster" = 10610L,
-            "Homo sapiens" = 19752L,
-            "Mus musculus" = 21093L
+            "Homo sapiens" = 19753L,
+            "Mus musculus" = 21090L
         ),
         FUN = function(organism, nrow) {
             object <- PANTHER(organism = organism, release = "11.0")
@@ -29,8 +29,8 @@ test_that("PANTHER : 12.0", {
         nrow = c(
             "Caenorhabditis elegans" = 14127L,
             "Drosophila melanogaster" = 10030L,
-            "Homo sapiens" = 19558L,
-            "Mus musculus" = 20882L
+            "Homo sapiens" = 19556L,
+            "Mus musculus" = 20884L
         ),
         FUN = function(organism, nrow) {
             object <- PANTHER(organism = organism, release = "12.0")
@@ -47,8 +47,8 @@ test_that("PANTHER : 13.0", {
         nrow = c(
             "Caenorhabditis elegans" = 13494L,
             "Drosophila melanogaster" = 10087L,
-            "Homo sapiens" = 18600L,
-            "Mus musculus" = 19821L
+            "Homo sapiens" = 18599L,
+            "Mus musculus" = 19822L
         ),
         FUN = function(organism, nrow) {
             object <- PANTHER(organism = organism, release = "13.0")
@@ -65,8 +65,8 @@ test_that("PANTHER : 13.1", {
         nrow = c(
             "Caenorhabditis elegans" = 13806L,
             "Drosophila melanogaster" = 10349L,
-            "Homo sapiens" = 19671L,
-            "Mus musculus" = 20929L
+            "Homo sapiens" = 19669L,
+            "Mus musculus" = 20930L
         ),
         FUN = function(organism, nrow) {
             ## NOTE C. elegans file here has parsing issues.
@@ -86,8 +86,8 @@ test_that("PANTHER : 14.0", {
         nrow = c(
             "Caenorhabditis elegans" = 14298L,
             "Drosophila melanogaster" = 10547L,
-            "Homo sapiens" = 19770L,
-            "Mus musculus" = 21058L
+            "Homo sapiens" = 19767L,
+            "Mus musculus" = 21059L
         ),
         FUN = function(organism, nrow) {
             object <- PANTHER(organism = organism, release = "14.0")
@@ -106,8 +106,8 @@ test_that("PANTHER : 16.0", {
         nrow = c(
             "Caenorhabditis elegans" = 14426L,
             "Drosophila melanogaster" = 10656L,
-            "Homo sapiens" = 19517L,
-            "Mus musculus" = 20984L
+            "Homo sapiens" = 19511L,
+            "Mus musculus" = 20986L
         ),
         FUN = function(organism, nrow) {
             object <- PANTHER(organism = organism, release = "16.0")
