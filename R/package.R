db565e00a69545e6baa906360e7804ea3d946553
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-25 17:04:10 -0400

    Move imports

--- R/package.R
+++ R/package.R
@@ -5,3 +5,43 @@
 #' @aliases NULL
 #' @keywords internal
 "_PACKAGE"
+
+
+
+## S4 classes ==================================================================
+
+#' @importClassesFrom S4Vectors DFrame
+NULL
+
+
+
+## S4 generics and methods =====================================================
+
+#' @importFrom AcidGenerics camelCase leftJoin mutateAt showHeader
+#' @importFrom BiocGenerics do.call lapply rbind sort unique unlist unsplit
+#' @importFrom IRanges gsub
+#' @importFrom S4Vectors complete.cases head metadata metadata<- split tail
+#' @importFrom methods show
+#' @importFrom pipette import
+#'
+#' @importMethodsFrom AcidBase showHeader
+#' @importMethodsFrom AcidPlyr leftJoin mutateAt
+#' @importMethodsFrom pipette import
+#' @importMethodsFrom syntactic camelCase
+NULL
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom AcidBase pasteURL showSlotInfo strExtract strMatch
+#' @importFrom AcidCLI abort alert alertWarning
+#' @importFrom AcidGenomes HGNC MGI
+#' @importFrom goalie assert hasInternet hasLength hasNoDuplicates hasRows
+#' isOrganism isString isSubset validate
+#' @importFrom IRanges CharacterList SplitDataFrameList
+#' @importFrom S4Vectors DataFrame SimpleList
+#' @importFrom methods as is new setClass setValidity
+#' @importFrom pipette cacheURL
+#' @importFrom utils packageName packageVersion
+NULL
